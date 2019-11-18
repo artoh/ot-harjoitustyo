36a6fff245b95c0bdf0f6ec667696e3f70d9f7e7
Arto Hyvättinen <arto.hyvattinen@gmail.com>
2019-11-18 09:04:40 +0200

    JavaFx aloitettu

---
+++ LaskeTunnit/src/main/java/artoh/lasketunnit/ui/LaskeTunnitApplication.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package artoh.lasketunnit.ui;
+
+import javafx.application.Application;
+import javafx.stage,Stage;
+
+/**
+ *
+ * @author arto
+ */
+public class LaskeTunnitApplication extends Application {
+    
+    @Override
+    public void start(Stage window) {
+        window.setTitle("Lasketunnit - UI not implementet!");
+        window.show();                
+    }
+    
+    public static void main(String[] args) {
+        launch(LaskeTunnitApplication.class);
+    }
+}
